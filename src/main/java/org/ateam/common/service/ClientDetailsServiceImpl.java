e15d2c2efcbdd7eaabeab358fcda0fb838a40473
Mayank Sharma <imptodefeat@gmail.com>
2015-08-11 06:16:18 +0530

    ClientDetails service set up

--- src/main/java/org/ateam/common/service/ClientDetailsServiceImpl.java
+++ src/main/java/org/ateam/common/service/ClientDetailsServiceImpl.java
@@ -1,11 +1,15 @@
 package org.ateam.common.service;
 
+import org.ateam.common.dao.ClientDAO;
 import org.ateam.common.model.Client;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.provider.BaseClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Map;
@@ -14,10 +18,31 @@ import java.util.Set;
 /**
  * Created by OPSKMC on 8/9/15.
  */
+@Service
 public class ClientDetailsServiceImpl implements ClientDetailsService {
+    @Autowired
+    ClientDAO dao;
     @Override
+    @Transactional
     public ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
-        Client client = new Client();
+        Client client = dao.loadClientByClientId(clientId);
         return client;
     }
+    @Transactional
+    public void saveOrUpdateClient(Client client){
+        dao.saveOrUpdateClient(client);
+    }
+    @Transactional
+    public void updateClient(Client client){
+        dao.updateClient(client);
+    }
+    @Transactional
+    public void getClientById(Integer id){
+        dao.getClientById(id);
+    }
+    @Transactional
+    public void deleteClient(Client client){
+        dao.deleteClient(client);
+    }
+
 }
